9c25245f7327252c7f869ed3b00e3cf9285ec5ed
DongHeon Lee <niche3600@gmail.com>
2023-01-14 10:19:19 +0900

    usersコントローラーの生成とルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/index', to: "users#index"
+  get 'users', to: "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
